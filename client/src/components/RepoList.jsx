8016cd8ca46040d404557b8e81652aeaf7b2c01d
Felipe <phil.sousa@gmail.com>
2019-01-24 19:54:15 -0800

    changed package jason to make heroku work

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -4,14 +4,14 @@ import ListItem from './ListItem.jsx'
 const RepoList = (props) => (
   <div>
     <h4> Repo List Component </h4>
-    
+
     There are {props.repos.length} repos.
     <ul>
       <h4>Results based on Forks count</h4>
       {props.repos.map((ele, ind) => {
         return <ListItem key = {ind} element = {ele} />;
-        
-    })}
+
+      })}
     </ul>
   </div>
 )
